3afae279f90d627c90a4e56ce3580edc0bd9b90b
Sharon <sharon.adami14@gmail.com>
2019-12-08 17:00:12 +0330

    Added a wrapping component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from "react";
-import Email from "./Components/Email";
-import CardNumber from "./Components/CardNumber";
-import Password from "./Components/Password";
-import SecurityCode from "./Components/SecurityCode";
-import Date from "./Components/Date";
+import Card from "./Components/Card";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardNumber />
-        <Password />
-        <SecurityCode />
-        <Date />
-        <Email />
+        <Card />
       </div>
     );
   }
