be175325eeee664de52875af54da0221184015ce
Sharon <sharon.adami14@gmail.com>
2019-12-08 15:18:03 +0330

    Used the components in the app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import Card from "./Components/Card";
+import Email from "./Components/Email";
+import CardNumber from "./Components/CardNumber";
+import Password from "./Components/Password";
+import SecurityCode from "./Components/SecurityCode";
+import Date from "./Components/Date";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello!</h1>
-        <Card />
+        <CardNumber />
+        <Password />
+        <SecurityCode />
+        <Date />
+        <Email />
       </div>
     );
   }
