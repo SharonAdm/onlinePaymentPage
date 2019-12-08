ac7818570dbc407b00957c958f73024ee6828570
Sharon <sharon.adami14@gmail.com>
2019-12-08 11:42:26 +0330

    Added the new component to the App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Card from "./Components/Card";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello!</h1>
+        <Card />
       </div>
     );
   }
