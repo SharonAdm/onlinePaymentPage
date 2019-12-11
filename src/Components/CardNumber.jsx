bad359e76133db89d00d35d65a87a0ea231ffe6a
Sharon <sharon.adami14@gmail.com>
2019-12-11 15:54:02 +0330

    Changed the input box .

--- src/Components/CardNumber.jsx
+++ src/Components/CardNumber.jsx
@@ -4,40 +4,54 @@ import "../style.css";
 class CardNumber extends Component {
   render() {
     return (
+      // <div className="container">
+      //   <label htmlFor="cardNumber">:شماره کارت</label>
+      //   <form className="form-inline">
+      //     <div className="form-group">
+      //       <input
+      //         className="form-control"
+      //         id="inputCardNumber"
+      //         type="text"
+      //         placeholder="xxxx"
+      //         maxLength="4"
+      //       ></input>
+      //       <p>_</p>
+      //       <input
+      //         className="form-control"
+      //         id="inputCardNumber"
+      //         type="text"
+      //         placeholder="xxxx"
+      //         maxLength="4"
+      //       ></input>
+      //       <p>_</p>
+      //       <input
+      //         className="form-control"
+      //         id="inputCardNumber"
+      //         type="text"
+      //         placeholder="xxxx"
+      //         maxLength="4"
+      //       ></input>
+      //       <p>_</p>
+      //       <input
+      //         className="form-control"
+      //         id="inputCardNumber"
+      //         type="text"
+      //         placeholder="xxxx"
+      //         maxLength="4"
+      //       ></input>
+      //     </div>
+      //   </form>
+      // </div>
       <div className="container">
-        <label htmlFor="cardNumber">:شماره کارت</label>
-        <form className="form-inline">
+        <form className="form">
           <div className="form-group">
+            <label htmlFor="cardNumber">: شماره کارت</label>
             <input
+              type="text"
               className="form-control"
               id="inputCardNumber"
-              type="text"
-              placeholder="xxxx"
-              maxLength="4"
-            ></input>
-            <p>_</p>
-            <input
-              className="form-control"
-              id="inputCardNumber"
-              type="text"
-              placeholder="xxxx"
-              maxLength="4"
-            ></input>
-            <p>_</p>
-            <input
-              className="form-control"
-              id="inputCardNumber"
-              type="text"
-              placeholder="xxxx"
-              maxLength="4"
-            ></input>
-            <p>_</p>
-            <input
-              className="form-control"
-              id="inputCardNumber"
-              type="text"
-              placeholder="xxxx"
-              maxLength="4"
+              placeholder="xxxx - xxxx - xxxx - xxxx"
+              maxLength="16"
             ></input>
           </div>
         </form>
