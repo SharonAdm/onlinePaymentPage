de2ea9e5d8f505e3bc0daf498b36d0925c8120af
Sharon <sharon.adami14@gmail.com>
2020-01-05 15:27:49 +0330

    Removed some unneccessary code.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, compose } from "redux";
+import { createStore } from "redux";
 import reducer from "./store/reducer";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-
-const composeEnhancers =
-  (typeof window !== "undefined" &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
 
 const store = createStore(
   reducer,
